bc28bdc8c63f32b58d23ae6f3c6d2065fbcc18f7
Edrick <ecarreon@77global.biz>
2022-08-29 15:35:44 +0800

    Dependent Dropdown

--- src/app/component/check-out/check-out.component.ts
+++ src/app/component/check-out/check-out.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
 import { CartService } from 'src/app/service/cart.service';
 
 @Component({
@@ -16,15 +17,24 @@ export class CheckOutComponent implements OnInit {
   public products: any = [];
   public grandTotal : number = 0;
   
-  constructor(private http : HttpClient, private cartService: CartService) { }
+  constructor(private auth: AuthService, private http : HttpClient, private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.city = this.auth.city();
     this.cartService.getUniqueProducts()
     .subscribe((data:any) => {
       console.log(data)
       this.products = data;
       this.grandTotal = this.cartService.getTotalPrice();
     });
+  }
+  city:any=[];
+  barangay:any=[];
+
+  onSelect(city: any) {
+    // console.log(city.target.value)
+    this.barangay = this.auth.barangay().filter(e => e.id == city.target.value);
+    console.log(this.barangay);
   }
 
   placeOrder(){
