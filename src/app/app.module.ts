31e054a60742b82b391ae02168d2df2bc1ef6eba
Hedrick28 <jpontilla@77global.biz>
2022-09-29 11:54:49 +0800

    Remove unnecessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CartComponent } from './component/cart/cart.component';
-import { HeaderComponent } from './component/header/header.component';
-import { ProductComponent } from './component/product/product.component';
-import { CategoriesComponent } from './component/categories/categories.component';
-import { FilterPipe } from './filter/filter.pipe';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SortPipe } from './filter/sort.pipe';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
-import { LoginComponent } from './component/login/login.component';
-import { SignupComponent } from './component/signup/signup.component';
-import { NavComponent } from './component/nav/nav.component';
-import { HomeComponent } from './component/home/home.component';
-
-import { ProfileComponent } from './component/profile/profile.component';
-import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
-
-import { PriceFilterPipe } from './filter/pricefilter.pipe';
-
-import { NotFoundComponent } from './component/not-found/not-found.component';
-import { CheckOutComponent } from './component/check-out/check-out.component';
-import { ProductAdminDashboardComponent } from './component/product-admin-dashboard/product-admin-dashboard.component';
-import { HasRoleGuard } from './guard/has-role.guard';
-import { PendingOrderComponent } from './component/pending-orders/pending-order.component';
-import { TopFivePipe } from './filter/topItems.pipe';
-
+import { HeaderComponent } from "./component/header/header.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AdminDashboardComponent } from "./component/admin-dashboard/admin-dashboard.component";
+import { LoginComponent } from "./component/login/login.component";
+import { SignupComponent } from "./component/signup/signup.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent,
     HeaderComponent,
-    ProductComponent,
-    CategoriesComponent,
-    FilterPipe,
-    SortPipe,
-    PriceFilterPipe,
     AdminDashboardComponent,
     LoginComponent,
     SignupComponent,
-    ForgotPasswordComponent,
-    NavComponent,
-    HomeComponent,
-    ProfileComponent,
-    ForgotPasswordComponent,
-    NotFoundComponent,
-    CheckOutComponent,
-    ProductAdminDashboardComponent,
-    PendingOrderComponent,
-    TopFivePipe
   ],
   imports: [
     BrowserModule,
@@ -61,8 +25,7 @@ import { TopFivePipe } from './filter/topItems.pipe';
     FormsModule,
     ReactiveFormsModule,
   ],
-  exports:[HeaderComponent],
-  providers: [HasRoleGuard],
-  bootstrap: [AppComponent]
+  exports: [HeaderComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
