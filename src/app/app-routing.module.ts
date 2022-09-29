fb1ada14fd942df38f97ab9516d4e3b141d5b368
angge28 <angelicagucela28@gmail.com>
2022-09-29 14:23:34 +0800

    student-dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
   {
     path: 'student',
     loadChildren: () => import('./modules/student/student.module').then((b) => b.StudentModule),
